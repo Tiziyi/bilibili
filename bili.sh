8376c303561eb3ff0e51f02cd75399a9b21804da
Tiziyi <91215756+Tiziyi@users.noreply.github.com>
2021-10-03 14:14:05 +0800

    Update bili.sh

--- bili.sh
+++ bili.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+/*
+================Loon==============
+[task_local]
+#运行海尔破
+31 2 * * *
+================Loon==============
+ */
 java -jar /ql/scripts/BILIBILI-HELPER.jar /ql/config/bilibili.json`
